34ee78f1325160f322e69fb18fc52c6cab79c3a2
menuin <menuin@hanyang.ac.kr>
2022-11-26 18:21:08 +0900

    commit server

---
+++ server/models/User.js
@@ -0,0 +1,95 @@
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const saltRounds = 10
+const secretToken = 'secretToken'
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        maxlength: 50
+    },
+    email: {
+        type: String,
+        trim: true,
+        unique: 1
+    },
+    password:{
+        type: String,
+        minlength: 5
+    },
+    role:{
+        type: Number
+    },
+    location:{
+        type: String
+    },
+    image: {
+        type: String
+    },
+    description: {
+        type: String
+    },
+    following: {
+        type: Array
+    },
+    token:{
+        type: String
+    },
+    tokenExp: {
+        type: Number
+    }
+})
+
+userSchema.pre('save', function(next) {
+    var user = this;
+
+    if(user.isModified('password')){
+        bcrypt.genSalt(saltRounds, function(err,salt){
+            if(err) {
+                return next(err);
+            }
+            bcrypt.hash(user.password, salt, function(err, hash){
+                if(err){
+                    return next(err);
+                }
+                user.password = hash
+                next();
+            })
+        })
+    }else{
+        next()
+    }
+})
+
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+        if(err) return cb(err);
+        cb(null, isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function(cb){
+    var user = this
+
+    var token = jwt.sign(user._id.toHexString(), secretToken)
+    user.token = token
+    user.save((err,user)=>{
+        if(err) return cb(err)
+        return cb(null,user)
+    })    
+}
+
+userSchema.statics.findByToken = function(token, cb){
+    var user = this
+    jwt.verify(token,secretToken,function(err, decoded){
+        user.findOne({"_id": decoded, "token": token}, function(err,user){
+            if(err) return cb(err);
+            cb(null,user)
+        })
+    })  
+}
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = { User }
